a88aad5e926e1b3fade0ce0f83e0d1e23f973ca3
IchiSamaFR <djordan-dsousa@hotmail.fr>
2020-08-21 10:41:08 +0200

    Ore gen changed

--- Assets/Scripts/NoiseMap.cs
+++ Assets/Scripts/NoiseMap.cs
@@ -7,6 +7,74 @@ public static class NoiseMap
     public static List<BlocToGen> Create(int width, int length, float scale, int xPos, int zPos, int seed, List<MapGenLevel> levels)
     {
         List<BlocToGen> mapTiles = new List<BlocToGen>();
+        int[,] mapTilesId = new int[16, 16];
+
+        float z = 0.0f;
+        float newXPos = xPos * scale;
+        float newZPos = zPos * scale;
+
+
+        //  Gen blank page
+        while (z < length)
+        {
+            float x = 0.0f;
+            while (x < width)
+            {
+                mapTilesId[(int)x, (int)z] = 0;
+
+                x++;
+            }
+            z++;
+        }
+
+        int i = 0;
+        foreach (MapGenLevel level in levels)
+        {
+            if(i != 0)
+            {
+                int[,] gen = GenLevel(width, length, scale, xPos, zPos, seed, 1 - level.level, i);
+
+                //  Already declared
+                z = 0.0f;
+                while (z < length)
+                {
+                    float x = 0.0f;
+                    while (x < width)
+                    {
+                        if(gen[(int)x, (int)z] > 0)
+                        {
+                            mapTilesId[(int)x, (int)z] = i;
+                        }
+                        x++;
+                    }
+                    z++;
+                }
+            }
+            i++;
+        }
+
+        z = 0.0f;
+        //  Gen blank page
+        while (z < length)
+        {
+            float x = 0.0f;
+            while (x < width)
+            {
+                mapTiles.Add(new BlocToGen(mapTilesId[(int)x, (int)z], (int)x, (int)z));
+
+                x++;
+            }
+            z++;
+        }
+
+        return mapTiles;
+    }
+
+    static int[,] GenLevel(int width, int length, float scale, int xPos, int zPos, int seed, float level, int id)
+    {
+        seed = (int)(seed * level * (id * 2f));
+
+        int[,] mapTilesId = new int[16, 16];
 
         float z = 0.0f;
         float newXPos = xPos * scale;
@@ -20,25 +88,16 @@ public static class NoiseMap
                 float xCoord = seed + newXPos + x / width * scale;
                 float zCoord = seed + newZPos + z / length * scale;
                 float yCoord = Mathf.PerlinNoise(xCoord, zCoord);
-                int id = 0;
 
-                float count = 0;
-                foreach (MapGenLevel map in levels)
+                if(yCoord > level)
                 {
-                    count += map.level;
-                    if (yCoord <= count)
-                    {
-                        break;
-                    }
-                    id++;
+                    mapTilesId[(int)x, (int)z] = id;
                 }
-
-                mapTiles.Add(new BlocToGen(id, (int)x, (int)z));
                 x++;
             }
             z++;
         }
 
-        return mapTiles;
+        return mapTilesId;
     }
 }
